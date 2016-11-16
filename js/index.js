5e492cdef8838cb81c9ea289314b6f25acc41a24
unknown <>
2016-11-16 10:51:29 +0800

    滚动特效

--- js/index.js
+++ js/index.js
@@ -1,20 +1,53 @@
-$(function(){
+$(function() {
 	//头部导航
-	var navListLis=$(".nav-list-li");
-	var navSecList=$(".nav-second-list");
-	navListLis.each(function(index,item){
-		$(this).hover(function(){
-			navSecList.eq(index).css("display","block")
-		},function(){
-			navSecList.eq(index).css("display","none")
+	var navListLis = $(".nav-list-li");
+	var navSecList = $(".nav-second-list");
+	navListLis.each(function(index, item) {
+		$(this).hover(function() {
+			navSecList.eq(index).css("display", "block")
+		}, function() {
+			navSecList.eq(index).css("display", "none")
 		})
 	});
 	//轮播图
 	var mySwiper = new Swiper('.swiper-container', {
-	autoplay: 2000,//可选选项，自动滑动
-	pagination : '.swiper-pagination',
-	paginationClickable :true,
-	loop : true,
-	autoplayDisableOnInteraction : false,
-	})
+		autoplay: 5000, //可选选项，自动滑动
+		pagination: '.swiper-pagination',
+		paginationClickable: true,
+		loop: true,
+		autoplayDisableOnInteraction: false,
+	});
+	//明星单品
+	(function() {
+		var leftBtn = $("#spanleft"),
+			rightBtn = $("#spanright"),
+			main2Wrap = $("#main2-wrap");
+		leftBtn.click(function() {
+			if (main2Wrap.position().left !== 0) {
+				main2Wrap.animate({
+					left: 0
+				})
+			}
+		})
+		rightBtn.click(function() {
+			if (!main2Wrap.position().left) {
+				main2Wrap.animate({
+					left: "-1238px"
+				})
+			}
+		})
+		//3s自动播放
+		setInterval(function(){
+			var l=main2Wrap.position().left;
+			if(l==0){
+				main2Wrap.animate({
+					left: "-1238px"
+				},1000)
+			}else{
+				main2Wrap.animate({
+					left: 0
+				},1000)
+			}
+		},5000)
+	})()
 })
